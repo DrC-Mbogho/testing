b9c10a2b5b78bf23ecce79dfb858acf929331c6d
DrC-Mbogho <116364889+DrC-Mbogho@users.noreply.github.com>
2024-06-12 18:46:13 +0300

    Update TestingGit.java
    added a print statement

--- TestingGit.java
+++ TestingGit.java
@@ -3,5 +3,6 @@ public class TestingGit
    public static void main(String arg[])
    {
      System.out.println ("Just testing Git and GitHub"); 
+     System.out.print("Adding a new line");
    }
 }
